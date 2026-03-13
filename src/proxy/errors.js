e569e49afd9895b41659af04cd1dccbce15da8a5
mephmanx <52037738+mephmanx@users.noreply.github.com>
2026-03-13 21:33:19 +0000

    Deploying to release-prod from @ dotcomrow/cf-suncoast-graphql-proxy@eb27e01f7c5c88a3a089d3b23707cf3850cd5df0 🚀

---
+++ src/proxy/errors.js
@@ -0,0 +1,11 @@
+export function toLoggableError(error) {
+  if (error instanceof Error) {
+    return {
+      name: error.name,
+      message: error.message,
+      stack: error.stack,
+    };
+  }
+
+  return { message: String(error) };
+}
